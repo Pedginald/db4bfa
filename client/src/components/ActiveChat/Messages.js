a1b47a4ef7ac710dd06d3664b0693ea1ddf1c7a8
Peter Rawlings <peterawlings24@hotmail.co.uk>
2022-02-18 11:54:56 -0500

    Passing down message, lastRead and otherUser props

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -31,7 +31,13 @@ const Messages = (props) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble 
+            key={message.id} 
+            message={message} 
+            time={time} 
+            lastReadMessageId={lastReadMessageId}
+            otherUser={otherUser}
+          />
         ) : (
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
         );
