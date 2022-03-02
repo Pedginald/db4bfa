339dd7db1d0370e23cc182198f4bbba50f374f27
Peter Rawlings <peterawlings24@hotmail.co.uk>
2022-03-02 17:08:05 -0500

    Creating many to many relation and through table

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,9 +4,12 @@ const Message = require("./message");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+Conversation.belongsToMany(User, {
+  through: "Participants"
+});
+User.belongsToMany(Conversation, {
+  through: "Participants"
+});
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
